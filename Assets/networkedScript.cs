5612127cbc3574bb287385d3ca6e7d8c46690c8a
ameuleman <andreasmeu@gmail.com>
2017-06-07 09:26:29 +0200

    Create project

---
+++ Assets/networkedScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class networkedScript : NetworkBehaviour {
+	[SyncVar]
+	public Color color;
+	private Material material;
+
+	void Start () {
+		material = new Material (Shader.Find ("Diffuse"));
+	}
+
+	void Update(){
+		material.color = color;
+		this.GetComponent<Renderer> ().material = material;
+	}
+
+	[ClientRpc]
+	public void RpcAction()
+	{
+		if (color == Color.black)
+			color = Color.blue;
+		else
+			color = Color.black;
+	}
+}
